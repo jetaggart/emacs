e9d4747b5c79d02df09a9701d4eb52fcc5e45923
Jeff Taggart <jeff@jetaggart.com>
2014-08-21 09:28:17 -0600

    Auto save constantly

--- init.el
+++ init.el
@@ -28,4 +28,25 @@
 (load "_web-mode.el")
 (load "_yasnippet.el")
 
+;; save buffers constantly
+(defadvice switch-to-buffer (before save-buffer-now activate)
+    (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+    (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+    (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+    (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+    (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+    (when buffer-file-name (save-buffer)))
+
+(defun save-all ()
+    (interactive)
+      (save-some-buffers t))
+
+(add-hook 'focus-out-hook 'save-all)
+(run-with-idle-timer 1 t 'save-all) 
+
 
