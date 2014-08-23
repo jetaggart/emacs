cc2f34b97a2d5fcda9fefd12e358d855d362311b
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 17:03:19 -0600

    Add stylish and tags to saving haskell files

--- custom/_haskell.el
+++ custom/_haskell.el
@@ -1,3 +1,8 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 (setq haskell-process-type 'cabal-repl)
+(custom-set-variables
+ ;; To enable tags generation on save.
+ '(haskell-tags-on-save t)
+ ;; To enable stylish on save.
+ '(haskell-stylish-on-save t))
