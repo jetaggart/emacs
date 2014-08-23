522418dd84a6e4c6b0232913e2a8027cc06625a2
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 15:25:43 -0600

    Added some bindings to evil

--- custom/_evil.el
+++ custom/_evil.el
@@ -17,3 +17,11 @@
   "h" 'magit-toggle-diff-refine-hunk)
 (evil-add-hjkl-bindings magit-log-mode-map 'emacs)
 (evil-add-hjkl-bindings magit-commit-mode-map 'emacs)
+
+
+;; Window commands
+(global-set-key (kbd "C-w") 'evil-window-map)
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
