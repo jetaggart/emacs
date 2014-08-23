aab0f77ca9f6ade9d4788c85b2cc6b89b05847ad
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 16:46:35 -0600

    Add advice macro and more evil tweaks

--- custom/_evil.el
+++ custom/_evil.el
@@ -1,10 +1,28 @@
 (evil-mode 1)
 (global-evil-surround-mode 1)
+(global-evil-leader-mode t)
 
 ;; Set the initial evil state that certain major modes will be in.
-(evil-set-initial-state 'nav-mode 'emacs)
-(evil-set-initial-state 'grep-mode 'emacs)
-(evil-set-initial-state 'ibuffer-mode 'normal)
+(defcustom my-evil-state-modes
+  '(fundamental-mode
+    nav-mode
+    ibuffer-mode
+    grep-mode
+    text-mode
+    prog-mode
+    sws-mode
+    dired-mode
+    comint-mode
+    log-edit-mode
+    compilation-mode)
+  "List of modes that should start up in Evil state."
+  :type '(repeat (symbol)))
+
+(defun my-enable-evil-mode ()
+  (if (apply 'derived-mode-p my-evil-state-modes)
+      (turn-on-evil-mode)
+    (set-cursor-color "red")))
+(add-hook 'after-change-major-mode-hook 'my-enable-evil-mode)
 
 (define-key evil-insert-state-map [remap newline] 'newline)
 (define-key evil-insert-state-map [remap newline-and-indent] 'newline-and-indent)
@@ -14,3 +32,22 @@
 (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
 (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
+(after 'evil
+       ;; fix conflict with electric-indent-mode in 24.4
+       (define-key evil-insert-state-map [remap newline] 'newline)
+       (define-key evil-insert-state-map [remap newline-and-indent] 'newline-and-indent)
+
+       (after "evil-leader-autoloads"
+	      (evil-leader/set-leader ",")
+	      (evil-leader/set-key
+		"w" 'save-buffer
+		"b d" 'kill-this-buffer
+		"v" (kbd "C-w v C-w l")
+		"s" (kbd "C-w s C-w j"))
+	      (after "magit-autoloads"
+		     (evil-leader/set-key
+		       "g s" 'magit-status
+		       "g b" 'magit-blame-mode
+		       "g c" 'magit-commit
+		       "g l" 'magit-log))))
--- init.el
+++ init.el
@@ -1,14 +1,25 @@
+;; after advice borrowed from https://github.com/bling/dotemacs
+(if (fboundp 'with-eval-after-load)
+    (defmacro after (feature &rest body)
+      "After FEATURE is loaded, evaluate BODY."
+      (declare (indent defun))
+      `(with-eval-after-load ,feature ,@body))
+  (defmacro after (feature &rest body)
+    "After FEATURE is loaded, evaluate BODY."
+    (declare (indent defun))
+    `(eval-after-load ,feature
+       '(progn ,@body))))
 ;; no frills
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
 
 (setq inhibit-startup-screen t)
 
-; disable backup
+;; disable backup
 (setq backup-inhibited t)
-; disable auto save
+;; disable auto save
 (setq auto-save-default nil)
-; auto-load all buffers on disk changes
+;; auto-load all buffers on disk changes
 (global-auto-revert-mode t)
 
 (require 'cask "/usr/local/Cellar/cask/0.7.0/cask.el")
@@ -32,21 +43,21 @@
 
 ;; save buffers constantly
 (defadvice switch-to-buffer (before save-buffer-now activate)
-    (when buffer-file-name (save-buffer)))
+  (when buffer-file-name (save-buffer)))
 (defadvice other-window (before other-window-now activate)
-    (when buffer-file-name (save-buffer)))
+  (when buffer-file-name (save-buffer)))
 (defadvice windmove-up (before other-window-now activate)
-    (when buffer-file-name (save-buffer)))
+  (when buffer-file-name (save-buffer)))
 (defadvice windmove-down (before other-window-now activate)
-    (when buffer-file-name (save-buffer)))
+  (when buffer-file-name (save-buffer)))
 (defadvice windmove-left (before other-window-now activate)
-    (when buffer-file-name (save-buffer)))
+  (when buffer-file-name (save-buffer)))
 (defadvice windmove-right (before other-window-now activate)
-    (when buffer-file-name (save-buffer)))
+  (when buffer-file-name (save-buffer)))
 
 (defun save-all ()
-    (interactive)
-      (save-some-buffers t))
+  (interactive)
+  (save-some-buffers t))
 
 (add-hook 'focus-out-hook 'save-all)
 
