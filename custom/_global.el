ec999d25cd35e77f1317371877c5ff2b98e326a6
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 20:48:55 -0600

    Move all auto saving into real auto save

--- custom/_global.el
+++ custom/_global.el
@@ -30,24 +30,3 @@
 (global-linum-mode t)
 (setq linum-format "%4d \u2502 ")
 
-;; save buffers constantly
-(defadvice switch-to-buffer (before save-buffer-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice other-window (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-up (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-down (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-left (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-right (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-
-(defun save-all ()
-  (interactive)
-  (save-some-buffers t))
-
-(add-hook 'focus-out-hook 'save-all)
-
-
--- custom/_real-auto-save.el
+++ custom/_real-auto-save.el
@@ -78,3 +78,24 @@
 
 (add-hook 'text-mode-hook 'turn-on-real-auto-save)
 (add-hook 'prog-mode-hook 'turn-on-real-auto-save)
+
+;; save buffers constantly
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+
+(add-hook 'focus-out-hook 'save-all)
+
