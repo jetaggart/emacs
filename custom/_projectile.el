590211c8e4ed5928ee985f7193b78ee3a6980122
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 20:22:37 -0600

    Remove grizzl

--- custom/_projectile.el
+++ custom/_projectile.el
@@ -1,8 +1,6 @@
 (projectile-global-mode)
 
-(require 'grizzl)
 (setq projectile-indexing-method 'native)
-(setq projectile-completion-system 'grizzl)
 (setq projectile-enable-caching t)
 
 (global-set-key (kbd "M-p") 'projectile-find-file)
