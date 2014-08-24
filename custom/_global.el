555a043701ba4051cc73927865dba7591129dd46
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 20:33:04 -0600

    Move global config to _global

--- custom/_global.el
+++ custom/_global.el
@@ -1,3 +1,27 @@
+;; after advice borrowed from https://github.com/bling/dotemacs
+(if (fboundp 'with-eval-after-load)
+    (defmacro after (feature &rest body)
+      "After FEATURE is loaded, evaluate BODY."
+      (declare (indent defun))
+      `(with-eval-after-load ,feature ,@body))
+  (defmacro after (feature &rest body)
+    "After FEATURE is loaded, evaluate BODY."
+    (declare (indent defun))
+    `(eval-after-load ,feature
+       '(progn ,@body))))
+;; no frills
+(dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
+  (when (fboundp mode) (funcall mode -1)))
+
+(setq inhibit-startup-screen t)
+
+;; disable backup
+(setq backup-inhibited t)
+;; disable auto save
+(setq auto-save-default nil)
+;; auto-load all buffers on disk changes
+(global-auto-revert-mode t)
+
 ;; Enable copy and pasting from clipboard
 (setq x-select-enable-clipboard t)
 
@@ -5,5 +29,25 @@
 
 (global-linum-mode t)
 (setq linum-format "%4d \u2502 ")
+
+;; save buffers constantly
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+
+(add-hook 'focus-out-hook 'save-all)
 
 
--- init.el
+++ init.el
@@ -1,27 +1,3 @@
-;; after advice borrowed from https://github.com/bling/dotemacs
-(if (fboundp 'with-eval-after-load)
-    (defmacro after (feature &rest body)
-      "After FEATURE is loaded, evaluate BODY."
-      (declare (indent defun))
-      `(with-eval-after-load ,feature ,@body))
-  (defmacro after (feature &rest body)
-    "After FEATURE is loaded, evaluate BODY."
-    (declare (indent defun))
-    `(eval-after-load ,feature
-       '(progn ,@body))))
-;; no frills
-(dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
-  (when (fboundp mode) (funcall mode -1)))
-
-(setq inhibit-startup-screen t)
-
-;; disable backup
-(setq backup-inhibited t)
-;; disable auto save
-(setq auto-save-default nil)
-;; auto-load all buffers on disk changes
-(global-auto-revert-mode t)
-
 (require 'cask "/usr/local/Cellar/cask/0.7.0/cask.el")
 (cask-initialize)
 (require 'pallet)
@@ -41,25 +17,4 @@
 (load "_smartparens.el")
 (load "_web-mode.el")
 (load "_yasnippet.el")
-
-;; save buffers constantly
-(defadvice switch-to-buffer (before save-buffer-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice other-window (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-up (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-down (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-left (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-(defadvice windmove-right (before other-window-now activate)
-  (when buffer-file-name (save-buffer)))
-
-(defun save-all ()
-  (interactive)
-  (save-some-buffers t))
-
-(add-hook 'focus-out-hook 'save-all)
-
 
