1e7aa2747e15cdc7e518e6f73a6df018a825f111
Jeff Taggart <jeff@jetaggart.com>
2014-08-23 20:36:14 -0600

    Go to anything with helm/projectile

--- custom/_projectile.el
+++ custom/_projectile.el
@@ -4,8 +4,7 @@
 (setq projectile-indexing-method 'native)
 (setq projectile-enable-caching t)
 
-(global-set-key (kbd "M-p") 'projectile-find-file)
-(global-set-key (kbd "M-b") 'projectile-switch-to-buffer)
+(global-set-key (kbd "M-p") 'helm-projectile)
 
 (after "evil-leader-autoloads"
   (evil-leader/set-key
