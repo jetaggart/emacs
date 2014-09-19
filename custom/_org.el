2161d38c0f9701500b945dd39ea4fc16b87ef970
Jeff Taggart <jeff@jetaggart.com>
2014-09-19 14:53:51 -0600

    Added org capture templates

--- custom/_org.el
+++ custom/_org.el
@@ -2,6 +2,28 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
-(setq org-image-actual-width '(400))
 
-(setq org-agenda-files '("~/Private/todo"))
+(setq org-image-actual-width '(400))
+(setq org-startup-indented t)
+(setq org-indent-mode t)
+(setq org-ellipsis " \u25b6" )
+(setq org-hide-leading-stars t)
+(setq org-special-ctrl-a/e t)
+
+(setq org-agenda-files '("~/org"))
+(setq org-default-notes-file (concat org-directory "/todo.org"))
+(define-key global-map "\C-cc" 'org-capture)
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/todo.org" "Inbox")
+	 "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+	 "* %?\nEntered on %U\n  %i\n  %a")))
+
+(custom-set-faces
+ '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+ )
