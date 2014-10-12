8b34fe5e60671a289c78dde6e5e9301402d39b09
Jeff Taggart <jeff@jetaggart.com>
2014-10-12 16:02:39 -0600

    Let org mode make headers big

--- custom/_org.el
+++ custom/_org.el
@@ -25,12 +25,5 @@
         ("j" "Journal" entry (file+datetree "~/org/journal.org")
 	 "* %?\nEntered on %U\n  %i\n  %a")))
 
-(custom-set-faces
- '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
-
 (add-hook 'org-mode-hook 'evil-org-mode) ;; only load with org-mode
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
