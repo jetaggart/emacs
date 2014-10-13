ed7f05f7c5817b12da9808bc22a5aa3ee2104b43
Jeff Taggart <jeff@jetaggart.com>
2014-10-13 10:30:28 -0600

    Remove custom set faces

--- init.el
+++ init.el
@@ -38,10 +38,3 @@
  '(safe-local-variable-values (quote ((hamlet/basic-offset . 4)))))
 
 (load-theme 'solarized-light)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
