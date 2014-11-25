621b3766a8be177f546871e76ba8a912cac38a20
psylinse <jeff@jetaggart.com>
2014-11-25 12:47:26 -0700

    Ignore smartparens in web mode

--- custom/_smartparens.el
+++ custom/_smartparens.el
@@ -1,8 +1,11 @@
+(setq sp-ignore-modes-list '(minibuffer-inactive-mode web-mode))
+
 (require 'smartparens-config)
 (require 'smartparens-ruby)
 
 (smartparens-global-mode)
 (show-smartparens-global-mode t)
+
 
 (sp-with-modes '(rhtml-mode)
   (sp-local-pair "<" ">")
