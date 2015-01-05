d526cb01d18fbd007a217ba7bc6cef18c3dc0d4a
Jeff Taggart <jeff@jetaggart.com>
2015-01-05 09:19:14 -0700

    Make eshell feel more natural

--- custom/_eshell.el
+++ custom/_eshell.el
@@ -17,3 +17,14 @@ directory to make multiple eshell windows easier."
     (eshell-send-input)))
 
 (global-set-key (kbd "C-!") 'eshell-here)
+
+(defun m-eshell-hook ()
+
+; define control p, control n and the up/down arrow
+  (define-key eshell-mode-map [(control p)] 'eshell-previous-matching-input-from-input)
+  (define-key eshell-mode-map [(control n)] 'eshell-next-matching-input-from-input)
+
+  (define-key eshell-mode-map [up] 'previous-line)
+  (define-key eshell-mode-map [down] 'next-line))
+
+(add-hook 'eshell-mode-hook 'm-eshell-hook)
