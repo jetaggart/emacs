37733a79cec81d73181747d23163affafe00c8d3
Jeff Taggart <jeff@jetaggart.com>
2015-03-13 15:07:19 -0600

    Set haml mode

--- custom/_ruby.el
+++ custom/_ruby.el
@@ -3,6 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
 
 (add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'ruby-mode-hook 'ruby-refactor-mode-launch)
