edbf131044b0dd89c95b368828747603b772030f
Jeff Taggart <jeff@jetaggart.com>
2015-03-13 16:28:08 -0600

    Fix halm finally

--- custom/_ruby.el
+++ custom/_ruby.el
@@ -3,8 +3,6 @@
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
-(add-to-list 'auto-mode-alist '("\\.html.haml$" . haml-mode))
 
 (add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'haml-mode-hook 'robe-mode)
