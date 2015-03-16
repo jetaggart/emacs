cc7d2084736f735014baa6cc4cc4e414a008ed96
Jeff Taggart <jeff@jetaggart.com>
2015-03-16 10:29:37 -0600

    Formatting

--- custom/_global.el
+++ custom/_global.el
@@ -151,6 +151,26 @@
                             (?\' . ?\')
                             ) )
 
+;; auto save on lost focus
+(defun save-all ()
+  (interactive)
+  (save-some-buffers t))
+
+(add-hook 'focus-out-hook 'save-all)
+
+(defadvice switch-to-buffer (before save-buffer-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice other-window (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-up (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-down (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-left (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+(defadvice windmove-right (before other-window-now activate)
+  (when buffer-file-name (save-buffer)))
+
 ;; emacs os x bindings
 (global-set-key (kbd "s-<left>") 'backward-word)
 (global-set-key (kbd "s-<right>") 'forward-word)
